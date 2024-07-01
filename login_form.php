5f4b64890afd578a5104242d16fd8e2f74d45e59
veda-kat <blackbullish47888@gmail.com>
2024-07-01 12:39:30 +0300

    stage

---
+++ login_form.php
@@ -0,0 +1,58 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_post['submit'])) {
+
+    $name= mysqli_real_escape_string($conn,$_POST['name']);
+    $email= mysqli_real_escape_string($conn,$_POST['email']);
+    $pass = md5($_POST['password']);
+    $cpass = md5($_POST['cpassword']);
+    $user_type = $_post['user_type'];
+    $select = "SELECT * FROM user_form WHERE email ='$email' && password ='$pass' ";
+    $result = mysqli_query($conn,$select);
+
+    if(mysqli_num_rows($result)> 0){
+        $row = mysqli_fetch_array($result);
+
+        if($row["user_type"]== 'admin'){
+            $_SESSION['admin_name'] = $row['name'];
+            header('location:admin_page.php');
+        }elseif($row['user_type']== 'user'){
+        $_SESSION['user_name'] = $row['name'];
+        header('location:user_page.php');
+    }
+}else{
+    $error[] = 'incorrect email or password!';
+}
+    
+};
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login form</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    
+<div class="form-container">
+    <form action="" method="post">
+        <h3>login now</h3>
+        <input type="email" name="email" required placeholder="enter your email">
+        <input type="password" name="password" required placeholder="enter your password">
+        
+        <input type="submit" name="submit" value="login now" class="form-btn">
+        <p>don't have an account? <a href="register_form.php">register now</a></p>
+</form>
+</div>
+
+</body>
+</html>  
